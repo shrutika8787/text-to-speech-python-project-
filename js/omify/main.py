d0abdfb948dffa21836f95d3981a9be9b19cac03
SHRUTIKA AGARWAL <114109014+shrutika8787@users.noreply.github.com>
2023-12-07 16:23:55 +0530

    Delete js directory

--- js/omify/main.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from pandas_profiling import ProfileReport
-
-df = pd.read_csv(r"c:\Users\Admin\Documents\project\js\omify\sheetnew.csv")
-
-print(df)
-
-profile = ProfileReport(df)
-profile.to_file(output_file="sheetnew.html")
--- js/omify/newmain.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from pandas_profiling import ProfileReport
-
-df = pd.read_csv(r"c:\Users\Admin\Documents\project\js\omify\Sheetlist.csv")
-
-print(df)
-
-profile = ProfileReport(df)
-profile.to_file(output_file="Sheetlist.html")
